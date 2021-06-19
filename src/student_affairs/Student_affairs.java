3953d11e5c085a3a7af4976bb320ce47ab96977f
AhmedRoshdy <ahmedroshdy01029@gmail.com>
2021-06-19 23:15:23 +0200

    Initial commit

---
+++ src/student_affairs/Student_affairs.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package student_affairs;
+
+/**
+ *
+ * @author OWNER
+ */
+public class Student_affairs {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+         Login x=new Login();
+        x.setSize(600, 500);
+        x.setLocation(600, 250);
+        x.setVisible(true);
+    }
+    
+}
